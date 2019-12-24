fb72c64018104f2916c5714fe85907a6cb03a077
Chris Frewin <frewin.christopher@gmail.com>
2019-12-24 20:28:30 +0100

    Updates

---
+++ precache-manifest.9164a189c1673a6c682d4abc132cb6a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f550de2adbf91aeae8227d73727be0f",
+    "url": "/christmas-gift/index.html"
+  },
+  {
+    "revision": "dc2a56ea1cde8378441d",
+    "url": "/christmas-gift/static/css/main.efd2ee41.chunk.css"
+  },
+  {
+    "revision": "94f922873ae9cbbb5837",
+    "url": "/christmas-gift/static/js/2.643db6cd.chunk.js"
+  },
+  {
+    "revision": "d9bc9aff9b81d31e8e002b5479cfe228",
+    "url": "/christmas-gift/static/js/2.643db6cd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dc2a56ea1cde8378441d",
+    "url": "/christmas-gift/static/js/main.9d5f617e.chunk.js"
+  },
+  {
+    "revision": "41ae8a128e44a51312a5",
+    "url": "/christmas-gift/static/js/runtime-main.9bcc9046.js"
+  },
+  {
+    "revision": "da356121d225fe5a60b17a6a421aa4a1",
+    "url": "/christmas-gift/static/media/snowflake.da356121.svg"
+  }
+]);
